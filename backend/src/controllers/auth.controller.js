0466fff88692106821d6cc190b7ec906ef271520
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-04-13 17:44:49 +0530

    Backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,8 +3,10 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
+  //1. Check for role.
   const { role } = req.body;
   try {
+    //2. Check for the required fields
     if (role === "mentor") {
       const {fullName, email, password, passingYear, currentCompany, college} = req.body;
       if (!fullName || !email || !password || !passingYear || !currentCompany || !college) {
@@ -16,22 +18,23 @@ export const signup = async (req, res) => {
         return res.status(400).json({ message: "All fields are required" });
       }
     }
-
+    //3. Check if password is less than 8 characters.
     if (password.length < 8) {
       return res
         .status(400)
         .json({ message: "Password must be atleast 8 characters" });
     }
-
+    //4. Check if user already exists
     const user = await User.findOne({ email });
     if (user) {
       return res
         .status(400)
         .json({ message: "User already exists. Proceed to login." });
     }
-
+    //5. Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+    
     let newUser;
     if (role === "mentor") {
         newUser = new User({
@@ -53,6 +56,39 @@ export const signup = async (req, res) => {
         college,
         role: "student",
       });
+    }
+    if(newUser){
+      //6. Generate JWT token here
+      await newUser.save();
+      generateToken(newUser._id, res);
+
+
+      //7.Send the user data
+      if(role === 'mentor'){
+        return res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          role: "mentor",
+          passingYear: newUser.passingYear,
+          currentCompany: newUser.currentCompany,
+          college: newUser.college,
+          profilePic: newUser.profilePic,
+          token,
+      })}
+      else{
+        return res.status(201).json({
+          _id: newUser._id,
+          fullName: newUser.fullName,
+          email: newUser.email,
+          role: "student",
+          graduationYear: newUser.graduationYear,
+          rollNo: newUser.rollNo,
+          college: newUser.college,
+          profilePic: newUser.profilePic,
+          token,
+        })
+      }
     }
   } catch (error) {
     console.log("Error in signup controller:", error.message);
