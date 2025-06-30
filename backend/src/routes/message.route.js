e73ef4b4d82d1df3e4928a1c47546546eadbc7d6
Sahil Mishra <sahil.mishra1512@gmail.com>
2025-06-30 19:40:17 +0530

    Backend

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,13 @@
 import express from "express"
+import { protectRoute } from "../middlewares/auth.middleware";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller";
 
 const router = express.Router();
 
 
-router.get("/")
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:/id", protectRoute, sendMessage);
+
+
 export default router;
